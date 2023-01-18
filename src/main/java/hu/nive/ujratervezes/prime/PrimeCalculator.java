7adbb04559f67a4e95276222b8161aa0d007d1a1
Sziku <105989158+Sziku@users.noreply.github.com>
2023-01-18 12:50:43 +0100

    implement min method

--- src/main/java/hu/nive/ujratervezes/prime/PrimeCalculator.java
+++ src/main/java/hu/nive/ujratervezes/prime/PrimeCalculator.java
@@ -1,5 +1,27 @@
 package hu.nive.ujratervezes.prime;
 
 public class PrimeCalculator {
-    
+    public int min(int[] inputArr){
+
+        if(inputArr == null) throw new IllegalArgumentException();
+        if(inputArr.length == 0) throw new IllegalArgumentException();
+
+        int min = inputArr[0];
+        for(int number : inputArr){
+            if(number < min){
+                min = number;
+            }
+        }
+
+        return min;
+    }
+
+
+    public boolean isPrime(int num){
+        return false;
+    }
+
+    public int[] multiplyPrimesWithMinimum(int[] inputArr){
+        return null;
+    }
 }
